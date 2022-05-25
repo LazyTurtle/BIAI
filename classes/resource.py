433c59be24c2d5b714417b774850bad58e525cee
Jhonny <jh.hueller@gmail.com>
2022-05-25 10:16:36 +0200

    Introduced gathering point agent

--- classes/resource.py
+++ classes/resource.py
@@ -4,17 +4,18 @@ from mesa.space import MultiGrid
 
 class ResourceModel(Model):
 
-    def __init__(self, width, height, num_collectors, num_resources):
-        print("init")
+    def __init__(self, width, height, num_collectors, num_resources, num_gathering_points=1):
         self.grid = MultiGrid(width, height, True)
         self.num_collectors = num_collectors
         self.num_resources = num_resources
+        self.num_gathering_points = num_gathering_points
         self.running = True
         self.setup()
 
     def setup(self):
         self.fill_env(Resource, self.num_resources)
         self.fill_env(Collector, self.num_collectors)
+        self.fill_env(GatheringPoint, self.num_gathering_points)
 
     def fill_env(self, agent_cl, n):
         for i in range(n):
@@ -29,11 +30,12 @@ class Collector(Agent):
         super(Collector, self).__init__(unique_id, model)
 
     def portrayal(self):
-        shape = {"Shape": "circle",
-                     "Color": "red",
-                     "Filled": "true",
-                     "Layer": 0,
-                     "r": 0.5}
+        shape = {
+            "Shape": "circle",
+            "Color": "red",
+            "Filled": "true",
+            "Layer": 0,
+            "r": 0.5}
         return shape
 
 
@@ -42,9 +44,26 @@ class Resource(Agent):
         super(Resource, self).__init__(unique_id, model)
 
     def portrayal(self):
-        shape = {"Shape": "circle",
-                     "Color": "green",
-                     "Filled": "true",
-                     "Layer": 0,
-                     "r": 0.5}
+        shape = {
+            "Shape": "circle",
+            "Color": "green",
+            "Filled": "true",
+            "Layer": 0,
+            "r": 0.5}
+        return shape
+
+
+class GatheringPoint(Agent):
+    def __init__(self, unique_id, model):
+        super(GatheringPoint, self).__init__(unique_id, model)
+
+    def portrayal(self):
+        shape = {
+            "Shape": "rect",
+            "Color": "black",
+            "Filled": "true",
+            "Layer": 0,
+            "w": 1,
+            "h": 1
+            }
         return shape
