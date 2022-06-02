e8a6ed1c3bf251866f5dc8be60c257ad5de1058d
paoulus <paolo.chiste-2@studenti.unitn.i>
2022-06-02 16:43:47 +0200

    Added draft for multiple agents evolution.

--- evolution.py
+++ evolution.py
@@ -14,7 +14,7 @@ def evolve(genomes, config):
     steps = 50
     width = 10
     height = 10
-    num_collectors = 1
+    num_collectors = 5
     num_resources = 5
     num_gathering_points = 1
 
@@ -27,23 +27,25 @@ def evolve(genomes, config):
         nn = create_phenotype_network(cppn, substrate)
         environment.reset()
         # TODO modify in order to train multiple agents in the same environment
-        agent = environment.agents(Collector)[0]
+        collector_agents = environment.agents(Collector)
         nn.reset() #  in case we do multiple trials we have to reset the rnn before each of one
         total_reward = 0.
-        for i in range(steps):
-            agent.update_sensors()
-            input_data = agent.get_sensor_data()
-            # The inputs are flattened in order to both have a list (required by neat) and to follow the order defined
-            # by the coordinates of hyper neat
-            input_data = input_data.flatten()
-            output = nn.activate(input_data)
-            action = np.argmax(output)
-            reward, done = environment.step(agent=agent, action=action)
-            total_reward += reward
-            if done:
-                break
-        final_reward = total_reward / (i + 1)  # we want to favour faster agents
-        genome.fitness = final_reward
+        agents_rewards = []
+        for agent in collector_agents:
+            for i in range(steps):
+                agent.update_sensors()
+                input_data = agent.get_sensor_data()
+                # The inputs are flattened in order to both have a list (required by neat) and to follow the order defined
+                # by the coordinates of hyper neat
+                input_data = input_data.flatten()
+                output = nn.activate(input_data)
+                action = np.argmax(output)
+                reward, done = environment.step(agent=agent, action=action)
+                total_reward += reward
+                if done:
+                    break
+            agents_rewards.append(total_reward / (i + 1))  # we want to favour faster agents
+        genome.fitness = sum(agents_rewards)
 
 
 if __name__ == '__main__':
