0b388b8e3ab31ad8b3520138c0d348fb52bfffb9
Jhonny <jh.hueller@gmail.com>
2022-06-29 14:36:19 +0200

    Introduced tag scheduler

---
+++ src/tag_scheduler.py
@@ -0,0 +1,25 @@
+from mesa.time import BaseScheduler
+from mesa import Model, Agent
+
+
+class TagScheduler(BaseScheduler):
+    def __init__(self, model: Model) -> None:
+        super(TagScheduler, self).__init__(model)
+        self.tagged_agents = dict()
+
+    def add(self, agent: Agent, tag=None):
+        super(TagScheduler, self).add(agent)
+        if tag is not None:
+            if tag in self.tagged_agents.keys():
+                self.tagged_agents[tag].add(agent)
+            else:
+                self.tagged_agents[tag] = [agent]
+
+    def step(self, tag=None):
+        if tag is None:
+            super(TagScheduler, self).step()
+        else:
+            for agent in self.tagged_agents[tag]:
+                agent.step()
+            self.steps += 1
+            self.time += 1
