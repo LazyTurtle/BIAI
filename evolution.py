8af33a87093169c2aa69679da7f516069ab0f695
Jhonny <jh.hueller@gmail.com>
2022-08-18 18:44:11 +0200

    Checkpoint

--- evolution.py
+++ evolution.py
@@ -89,7 +89,6 @@ def best_agent():
         CONFIG["network"]["substrate_output_dims"],
         CONFIG["network"]["substrate_sheet_dims"])
 
-
     draw_net(best_cppn, filename="reports/champion_images/cppn")
     draw_net(substrate, filename="reports/champion_images/substrate")
 
